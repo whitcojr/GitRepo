566affa20ba9b5b455b52dd644e8555e7a14d82b
whitcojr <whitcojr@mail.uc.edu>
2025-03-27 11:51:32 -0400

    Add project files.

---
+++ GitRepo/Apple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitRepo
+{
+    internal class Apple
+    {
+
+        private string _color;
+        private double _price;
+
+        // Create getter and a setter for the private properties
+        public double Price { get { return _price; } set { _price = value; } }
+        public string Color { get { return _color; } set { _color = value; } }
+    }
+}
---
+++ GitRepo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple myApple = new Apple(); // Declare and instantiate
+            myApple.Price = 2.25; // Invoke the setter
+            Console.WriteLine("Price of my apple is " + myApple.Price); // getter
+            myApple.Color = "red";
+            Console.WriteLine("Color of my apple is " + myApple.Color);
+        }
+    }
+}
